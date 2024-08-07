851d4e7509b10162d46e2da2cb121bc31cbad4f9
Steven Tjoa <steventjoa89@gmail.com>
2024-08-07 21:33:44 +0700

    refactoring HorizontalScrollableGridView.

--- src/components/GridView/HorizontalScrollableGridView.js
+++ src/components/GridView/HorizontalScrollableGridView.js
@@ -1,42 +1,15 @@
-import React, { useRef } from "react";
+import React from "react";
 import Card from "../Card/Card";
-import ArrowBackIosOutlinedIcon from "@mui/icons-material/ArrowBackIosOutlined";
-import ArrowForwardIosOutlinedIcon from "@mui/icons-material/ArrowForwardIosOutlined";
 
 function HorizontalScrollableGridView({ perfumeList }) {
-  const scrollContainerRef = useRef(null);
-
-  const scrollLeft = () => {
-    if (scrollContainerRef.current) {
-      scrollContainerRef.current.scrollBy({ left: -200, behavior: "smooth" });
-    }
-  };
-
-  const scrollRight = () => {
-    if (scrollContainerRef.current) {
-      scrollContainerRef.current.scrollBy({ left: 200, behavior: "smooth" });
-    }
-  };
-
   return (
-    <div className="relative w-full">
-      <div className="flex items-center justify-between">
-        <button className="z-10" onClick={scrollLeft}>
-          <ArrowBackIosOutlinedIcon />
-        </button>
-        <div
-          ref={scrollContainerRef}
-          className="flex w-[92vw] overflow-x-auto space-x-2 no-scrollbar"
-        >
-          {perfumeList.map((item, key) => (
-            <div key={key} className="flex-shrink-0 w-1/2 md:w-1/3 lg:w-1/4">
-              <Card item={item} isLarge={true} isShowPrice={false} />
-            </div>
-          ))}
-        </div>
-        <button className="z-10" onClick={scrollRight}>
-          <ArrowForwardIosOutlinedIcon />
-        </button>
+    <div className="w-full overflow-x-auto whitespace-nowrap no-scrollbar">
+      <div className="flex items-center">
+        {perfumeList.map((item, index) => (
+          <div key={index} className="flex-shrink-0 mx-2">
+            <Card item={item} isLarge={true} isShowPrice={false} />
+          </div>
+        ))}
       </div>
     </div>
   );
