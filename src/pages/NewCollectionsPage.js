377b138f6c5d80c142cb3c3543797b93670f5641
Steven Tjoa <steventjoa89@gmail.com>
2024-08-07 08:01:13 +0700

    refactored new collections page.

--- src/pages/NewCollectionsPage.js
+++ src/pages/NewCollectionsPage.js
@@ -1,45 +1,28 @@
 import React, { useEffect, useState } from "react";
-import NavbarFooterMenu from "../components/Navbar/NavbarFooterMenu";
+import Breadcrumb from "../components/Breadcrumb/Breadcrumb";
 import GridView from "../components/GridView/GridView";
 import { PERFUME_CATALOG } from "../data/data";
-import { SortOptions } from "../enums/sortOptionsEnum";
-import { sortPerfumes } from "../utils/sortingUtil";
 
 function NewCollectionsPage() {
-  const [page, setPage] = useState(1);
-  const [sortedBy, setSortedBy] = useState(SortOptions.DEFAULT);
-  const [sortedData, setSortedData] = useState([]);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    // Filter and sort data based on the selected sort option
-    const newCollections = PERFUME_CATALOG.sort(
-      (a, b) => new Date(b.releaseAt) - new Date(a.releaseAt)
-    ).slice(0, 4);
-
-    // Sort data based on the selected sorting option
-    let sorted = [...newCollections];
-    sorted = sortPerfumes(sortedBy, sorted);
-
-    // Set the sorted data
-    setSortedData(sorted);
-  }, [sortedBy]);
+    setData(
+      PERFUME_CATALOG.sort(
+        (a, b) => new Date(b.releaseAt) - new Date(a.releaseAt)
+      ).slice(0, 4)
+    );
+  }, []);
 
   return (
     <div>
-      {/* Navbar Footer */}
-      <NavbarFooterMenu
-        title="New Collections"
-        defaultSortOptionName="Most Popular"
-        sortedBy={sortedBy}
-        setSortedBy={setSortedBy}
-      />
+      <Breadcrumb title="New Collections" isShowFilter={false} />
 
       {/* Grid View: Content Area */}
       <GridView
         title="Our Exclusive New Collections"
-        page={page}
-        setPage={setPage}
-        data={sortedData}
+        data={data}
+        isShowPrice={false}
       />
     </div>
   );
