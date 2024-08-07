90c57fc221d9f470722f5a3d6bf600a98052e714
Steven Tjoa <steventjoa89@gmail.com>
2024-08-07 21:35:32 +0700

    Fixing HorizontalScrollableGridView marginx.

--- src/components/GridView/HorizontalScrollableGridView.js
+++ src/components/GridView/HorizontalScrollableGridView.js
@@ -6,7 +6,7 @@ function HorizontalScrollableGridView({ perfumeList }) {
     <div className="w-full overflow-x-auto whitespace-nowrap no-scrollbar">
       <div className="flex items-center">
         {perfumeList.map((item, index) => (
-          <div key={index} className="flex-shrink-0 mx-2">
+          <div key={index} className="flex-shrink-0 mx-2 md:mx-3 xl:mx-4">
             <Card item={item} isLarge={true} isShowPrice={false} />
           </div>
         ))}
